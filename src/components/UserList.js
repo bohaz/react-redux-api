8f5d359fc072fb7eab9ffa2e34815ba3c55815ae
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-07-26 14:32:32 -0400

    Set up project

---
+++ src/components/UserList.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers } from '../store/users/usersSlice';
+
+const UserList = () => {
+  const dispatch = useDispatch();
+  const { users, isLoading, error } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.login.uuid}>
+            {user.name.first} {user.name.last}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
---
+++ src/store/usersSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await fetch('https://randomuser.me/api/?results=10');
+  const data = await response.json();
+  return data.results;
+});
+
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: null,
+};
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default usersSlice.reducer;
