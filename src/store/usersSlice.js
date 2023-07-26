55362eb1dfd28a15a6993ce76eab904bbe057086
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-07-26 14:34:11 -0400

    Update API

--- src/store/usersSlice.js
+++ src/store/usersSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-  const response = await fetch('https://randomuser.me/api/?results=10');
+  const response = await fetch('https://randomuser.me/api/?results=5');
   const data = await response.json();
   return data.results;
 });
